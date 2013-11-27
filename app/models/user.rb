a381e244cdf1e1d67dc8d8560182006af2bb4c3a
Tiago Martins <tiago@headblendr.com>
2013-11-27 13:49:41 +0000

    Configured user model, added facebook credentials, modified devise initializer for omniauth, added the abook rb file, generated the migration with additional omniauth fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,39 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+   def self.find_or_create_from_auth_hash(auth_hash)
+   	find_by_auth_hash(auth_hash) || create_from_auth_hash(auth_hash)
+   end
+
+   def self.find_by_auth_hash(auth_hash)
+   	where(
+   		provider: auth_hash.provider,
+   		uid: auth_hash.uid
+   		).first
+   end
+
+   def self.create_from_auth_hash(auth_hash)
+   	create(
+   		provider: auth_hash.provider,
+   		uid: auth_hash.uid,
+   		email: auth_hash.info.email,
+   		name: auth_hash.info.name,
+   		oauth_token: auth_hash.credentials.token,
+   		oauth_expires_at: Time.at(auth_hash.credentials.expires_at)
+   		)
+   end
+
+   def password_required?
+   	super && provider.blank?
+   end
+
+   def update_with_password(params, *options)
+   	if encrypted_password.blank?
+   		update_attributes(params, *options)
+   	else
+   		super
+   	end
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131126160332) do
+ActiveRecord::Schema.define(version: 20131127134728) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20131126160332) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
